a0ad31ee36df03034b4250b3ec8457a3d433fda9
Victor Ospina <vospina@alltic.co>
2024-12-20 15:42:51 -0500

    open project

---
+++ src/main/java/com/example/electronicinvoice/ElectronicInvoiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.electronicinvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicInvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicInvoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/electronicinvoice/ElectronicInvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.electronicinvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicInvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
